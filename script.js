ff000527dcc5f5d494f2ac68ee7ec97eb9460344
Andrei Benea <andrei.benea89@gmail.com>
2024-10-05 16:52:10 -0400

    add js function to display dropdown on mobile

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const phoneInput = document.getElementById("phone");
+    if (document.title === "Portfolio :: Contact") {
+        const phoneInput = document.getElementById("phone");
 
-    phoneInput.addEventListener("input", function (event) {
-        let input = phoneInput.value.replace(/\D/g, '');
-        if (input.length > 3 && input.length <= 6) {
-            phoneInput.value = input.slice(0, 3) + '-' + input.slice(3);
-        } else if (input.length > 6) {
-            phoneInput.value = input.slice(0, 3) + '-' + input.slice(3, 6) + '-' + input.slice(6, 10);
-        } else {
-            phoneInput.value = input;
+        phoneInput.addEventListener("input", function (event) {
+            let input = phoneInput.value.replace(/\D/g, '');
+            if (input.length > 3 && input.length <= 6) {
+                phoneInput.value = input.slice(0, 3) + '-' + input.slice(3);
+            } else if (input.length > 6) {
+                phoneInput.value = input.slice(0, 3) + '-' + input.slice(3, 6) + '-' + input.slice(6, 10);
+            } else {
+                phoneInput.value = input;
+            }
+        });
+    }
+
+    const caret = document.getElementById("caret")
+    console.log(caret)
+    caret.addEventListener("click", (event) => {
+        const dropdownContent = document.getElementById("dropdown-content")
+        const mobileScreen = window.matchMedia("(max-width: 426px)").matches
+        if (mobileScreen) {
+            if (dropdownContent.style.display == "none") {
+                dropdownContent.style.display = "flex"
+                dropdownContent.style.flexDirection = "column"
+            }
+            else {
+                dropdownContent.style.display = "none"
+            }
         }
-    });
+    })
 });
